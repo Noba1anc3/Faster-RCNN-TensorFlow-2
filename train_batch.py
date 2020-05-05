64084cbb41f017549969701c580297b7169076ff
Noba1anc3 <zxryhjp@yahoo.co.jp>
2020-05-05 23:22:35 +0800

    val dataset non-fixed

--- train_batch.py
+++ train_batch.py
@@ -52,7 +52,7 @@ train_dataset = coco.CocoDataSet(dataset_dir='dataset', subset='train',
                                  mean=img_mean, std=img_std,
                                  scale=(800, 1216))
 test_dataset = coco.CocoDataSet(dataset_dir='dataset', subset='val',
-                                flip_ratio=flip_ratio, pad_mode='fixed',
+                                flip_ratio=flip_ratio, pad_mode='non-fixed',
                                 mean=img_mean, std=img_std,
                                 scale=(800, 1216))
 
