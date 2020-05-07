1505598829b4d367633e6365d6ded3eb2cec7706
NoBa1anc3 <zxryhjp@yahoo.co.jp>
2020-05-08 07:47:14 +0800

    Update train_batch.py

--- train_batch.py
+++ train_batch.py
@@ -69,7 +69,7 @@ test_dataset = coco.CocoDataSet(dataset_dir='dataset', subset='val',
 train_generator = data_generator.DataGenerator(train_dataset)
 train_tf_dataset = tf.data.Dataset.from_generator(
     train_generator, (tf.float32, tf.float32, tf.float32, tf.int32))
-train_tf_dataset = train_tf_dataset.batch(batch_size).prefetch(100).shuffle(100)
+train_tf_dataset = train_tf_dataset.batch(batch_size).prefetch(100)#.shuffle(100)
 
 num_classes = len(train_dataset.get_categories())
 model = faster_rcnn.FasterRCNN(num_classes=num_classes)
